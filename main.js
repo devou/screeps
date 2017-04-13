ea93af985fe6bfdd995ff463bc67a9f32476e02b
derivz <derivz@gmail.com>
2017-04-13 16:10:35 -0500

    add constants

--- main.js
+++ main.js
@@ -13,28 +13,29 @@ module.exports.loop = function () {
         }
     }
 
-    let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    let upgraiders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    let builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    let towerFillers = _.filter(Game.creeps, (creep) => creep.memory.role == 'towerFiller');
-    Memory.harvester_count = harvesters.length;
+    let creeps = _.groupBy(Game.creeps, 'memory.role')
+    let harvestersCount = creeps['harvester'] && creeps['harvester'].length || 0;
+    let upgraidersCount = creeps['upgrader'] && creeps['upgrader'].length || 0;
+    let buildersCount = creeps['builder'] && creeps['builder'].length || 0;
+    let towerFillersCount = creeps['towerFiller'] && creeps['towerFiller'].length || 0;
+    Memory.harvesterCount = harvestersCount;
 
-    if(harvesters.length == 0) {
+    if(harvestersCount === 0) {
         utils.createCreep('harvester', true);
     } else {
-        if(harvesters.length < 2) {
+        if(harvestersCount < 1) {
             utils.createCreep('harvester');
-        }else if(upgraiders.length < 1) {
+        }else if(upgraidersCount < 1) {
             utils.createCreep('upgrader');
-        }else if(builders.length < 1) {
+        }else if(buildersCount < 1) {
             utils.createCreep('builder');
-        }else if(towerFillers.length < 1) {
+        }else if(towerFillersCount < 1) {
             utils.createCreep('towerFiller');
         }
     }
 
     if(Game.spawns['Spawn1'].spawning) {
-        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        let spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
             '🛠️' + spawningCreep.memory.role,
             Game.spawns['Spawn1'].pos.x + 1,
@@ -46,17 +47,17 @@ module.exports.loop = function () {
 
     for(let name in Game.creeps) {
         let creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if(creep.memory.role === 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        if(creep.memory.role === 'upgrader') {
             roleUpgrader.run(creep);
             // roleBuilder.run(creep);
         }
-        if(creep.memory.role == 'builder') {
+        if(creep.memory.role === 'builder') {
             roleBuilder.run(creep);
         }
-        if(creep.memory.role == 'towerFiller') {
+        if(creep.memory.role === 'towerFiller') {
             roleTowerFiller.run(creep);
         }
     }
