98141b276f5b6c88cc7bbba6ede9bfc4fc3ee553
derivz <derivz@gmail.com>
2017-04-19 16:29:38 -0500

    tower fix

--- tower.js
+++ tower.js
@@ -5,16 +5,16 @@ module.exports = {
 	towerLogic: function(room=con.room) {
 	    let towers = room.find(FIND_STRUCTURES, {filter: st => st.structureType === STRUCTURE_TOWER});
 	    for (let tower of towers) {
-            let dStructures = _.sortByOrder(tower.room.find(FIND_STRUCTURES, {
-                filter: structure => (structure.hits <= structure.hitsMax - 900) && structure.hits < 60000
-            }), ['hits']);
-            if (dStructures.length > 0 && tower.energy > 500) {
-                tower.repair(dStructures[0]);
-            }
-
             let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
             if (closestHostile) {
                 tower.attack(closestHostile);
+            } else{
+                let dStructures = _.sortByOrder(tower.room.find(FIND_STRUCTURES, {
+                    filter: structure => (structure.hits <= structure.hitsMax - 900) && structure.hits < 60000
+                }), ['hits']);
+                if (dStructures.length > 0 && tower.energy > 500) {
+                    tower.repair(dStructures[0]);
+                }
             }
         }
 	}
